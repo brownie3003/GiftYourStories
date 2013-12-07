49e6f59787d33b34e3982e507f17f50a939f5d73
Andy Brown <brownie3003@gmail.com>
2013-12-07 01:40:10 +0000

    Change app.listen to Heroku example

--- app.js
+++ app.js
@@ -21,4 +21,7 @@ app.get('/', function (req, res) {
     res.end("Hi there!");
 })
 
-app.listen(3000);
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
