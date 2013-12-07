54b7aa2399ad8b24398ad266bc8d8a10227cad75
Andy Brown <brownie3003@gmail.com>
2013-12-07 19:07:03 +0000

    Signup form started

--- app.js
+++ app.js
@@ -9,12 +9,16 @@ function compile(str, path) {
 }
 
 app.set('views', __dirname + '/views');
+
 app.set('view engine', 'jade');
+
 app.use(express.logger('dev'));
+
 app.use(stylus.middleware({
     src: __dirname + "/public", compile: compile
     })
 );
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
@@ -22,6 +26,10 @@ app.get('/', function (req, res) {
   { title : 'Home' }
   )
 })
+
+app.get('/signup', function (req, res) {
+    res.render('signup');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
