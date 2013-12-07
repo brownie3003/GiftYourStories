bff520cc171a4983614a9a8fbd3559be9bb7c16d
Andy Brown <brownie3003@gmail.com>
2013-12-07 01:28:45 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    stylus = require('stylus');
+
+var app = express();
+
+function compile(str, path) {
+    return stylus(str)
+        .set('filename', path)
+}
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(stylus.middleware({
+    src: __dirname + "/public", compile: compile
+    })
+);
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.end("Hi there!");
+})
+
+app.listen(3000);
