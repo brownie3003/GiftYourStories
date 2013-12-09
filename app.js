ebaca3d45c443260e1beee4330d4137c3f5b6c20
Nicholas Angeli <nicangeli@gmail.com>
2013-12-09 17:14:53 +0000

    sends data to parse.com

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 var express = require('express'),
-    stylus = require('stylus');
+    stylus = require('stylus'),
+    Parse = require('node-parse-api').Parse;
+
+var APP_ID = "ey1VPmwuX0y8XmhbwqmP1fMPGE8CZWmiQLpQBOpo";
+var MASTER_KEY = "ZjPUYyvpN6MzHJH2QEsh8ENewsS1DTtEKtxnZUyQ";
+
+var parseApp = new Parse(APP_ID, MASTER_KEY);
 
 var app = express();
 
@@ -11,7 +17,7 @@ function compile(str, path) {
 app.set('views', __dirname + '/views');
 
 app.set('view engine', 'jade');
-
+app.use(express.bodyParser());
 app.use(express.logger('dev'));
 
 app.use(stylus.middleware({
@@ -29,8 +35,26 @@ app.get('/signup', function (req, res) {
     res.render('signup');
 });
 
-app.post('/step2', function(req, res) {
-  var data = req.body;
+app.post('/step2', function (req, res) {
+  //var data = req.body;
+  //console.log(data.theirName);
+  res.render('signup2', {theirName: req.body.theirName});
+});
+
+app.post('/step3', function(req, res) {
+  // upload the details and generate the unique email address here
+  var theirName = req.body.theirName,
+      yourName = req.body.yourName,
+      yourEmail = req.body.yourEmail,
+      customEmail = "for" + theirName.replace(/ /g, '') + "@giftyourstories.com";
+
+  parseApp.insert('User', {"theirName": theirName, "originatorName": yourName, "originatorEmail": yourEmail, "customEmail": customEmail}, function(err, response) { 
+    if(err) throw err; 
+    console.log(response);
+  });
+  res.render('signup3', {theirName: theirName, yourName: yourName, yourEmail: yourEmail, customEmail: customEmail});
+
+
 });
 
 var port = process.env.PORT || 5000;
